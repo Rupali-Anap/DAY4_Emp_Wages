c992eec3ca8298173e189f17be4b11f9374b8f10
Rupali-Anap <anaprupali333@gmail.com>
2022-07-09 02:42:27 +0530

    Prob3 solution is added

---
+++ Emp_Parttime_Wage.java
@@ -0,0 +1,23 @@
+package Day4_Pract_Prob;
+
+public class Emp_Parttime_Wage {
+
+        public static void main(String[] args) {
+            int PART_TIME = 1;
+            int FULL_TIME = 2;
+            int WAGE_PER_HR = 20;
+            int empType = (int) (Math.random() * 100) % 3;
+            int workingHrs = 0;
+            if (empType == FULL_TIME)
+            {            System.out.println("Employee Present for Full time");
+                workingHrs = 8;        }
+            else if (empType == PART_TIME)
+            {            System.out.println("Employee Present for Part time");
+                workingHrs = 4;        }
+            else       	 {
+                System.out.println("Employee is Absent");
+            }        int wage = workingHrs * WAGE_PER_HR;
+            System.out.println("Employee Daily Wage is " + wage);
+        }
+
+}
