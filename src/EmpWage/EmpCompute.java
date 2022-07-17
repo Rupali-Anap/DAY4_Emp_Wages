36038da8c9e2cf87efaac931b4914b004f999ba3
Rupali-Anap <anaprupali333@gmail.com>
2022-07-17 19:42:21 +0530

    Prob1 solution is added

---
+++ src/EmpWage/EmpCompute.java
@@ -0,0 +1,30 @@
+package EmpWage;
+
+public class EmpCompute {
+    int empPresent = 1;
+    double empCheck;
+
+    public EmpCompute() {
+        empCheck = Math.floor(Math.random() * 10) % 3 + 1;
+    }
+
+    public void present_absent() {
+        System.out.println("welcome to employee wage computation");
+        if (empCheck == empPresent) {
+            System.out.println("The Employee is Present");
+
+        } else if (empCheck == 2) {
+            System.out.println("The Employee is Absent");
+        } else {
+            System.out.println("The employee is present but he is part time");
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        EmpCompute emp = new EmpCompute();
+        emp.present_absent();
+
+    }
+}
